2ccbf4870f0464632aad27bbfcd07c853d9eeafb
saurabhbhatt31 <saurabh.bhatt@hestabit.in>
2024-01-22 16:08:40 +0530

    added error and response handler

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import dotenv from "dotenv";
 import connectDB from "./db/dbConnect.js";
-
+import app from "./app.js";
+dotenv.config();
+const port = process.env.PORT || 5000;
 dotenv.config({ path: ".env" });
-connectDB();
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on PORT : ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Failed : ", error);
+  });
