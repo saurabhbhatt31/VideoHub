0f7161ee091e2f560fbfc6b651240faa8a2ffd06
saurabhbhatt31 <saurabh.bhatt@hestabit.in>
2024-01-30 20:24:08 +0530

    added like and comment model

---
+++ src/models/like.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new Schema(
+  {
+    comment: {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    likedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    tweet: {
+      type: Schema.Types.ObjectId,
+      ref: "Tweet", //we've to add tweet model first
+    },
+  },
+  { timestamps: true }
+);
+
+export const like = mongoose.model("like", likeSchema);
